cd25cf8cf7d0f6e9fc0212347ae34ea8450585b5
Sandy <92988108+SKolackova@users.noreply.github.com>
2022-03-14 09:09:18 +0000

    Přidání tabulky na vyhodnocení

--- BMI.py
+++ BMI.py
@@ -1,7 +1,11 @@
 def main():
   hmotnost, vyska = otazka()
   print("Tvoje BMI je", hmotnost / (vyska * vyska))
-  
+  print('''BMI	Hodnocení
+méně než 18,5	Podváha
+18,5 až 24,9	Normální hmotnost
+25 až 29,9	Nadváha
+30 a více	Obezita''')
 
 def otazka():
   hmotnost = float(input("Zadej svoji váho v kilogramech: "))

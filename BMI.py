daba86c84698bace853df580530fb5cf2a7bcdff
Sandy <92988108+SKolackova@users.noreply.github.com>
2022-03-14 11:59:35 +0000

    Progres v odpovědi

--- BMI.py
+++ BMI.py
@@ -1,11 +1,19 @@
 def main():
   hmotnost, vyska = otazka()
-  print("Tvoje BMI je", hmotnost / (vyska * vyska))
+  BMI = hmotnost / (vyska * vyska)
+  if BMI <= 18.5:
+    print("Tvoje BMI je", BMI, ". Máš podváhu." )
+  elif BMI > 18.5 and BMI < 24.5:
+    print("Tvoje BMI je", BMI,". Máš normální hmotnost.")
+
+  
+  print("Tvoje BMI je", BMI )
   print('''BMI	Hodnocení
 méně než 18,5	Podváha
 18,5 až 24,9	Normální hmotnost
 25 až 29,9	Nadváha
 30 a více	Obezita''')
+  
 
 def otazka():
   hmotnost = float(input("Zadej svoji váho v kilogramech: "))
